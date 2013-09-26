10bde6e83b532d9ee75294777b6dd1e7bacab518
Geoff Lang <geoff@vonture.net>
2013-09-25 20:51:44 -0400

    Add a real class to encapsulate floating or fixed point values.

--- src/fixed_point.hpp
+++ src/fixed_point.hpp
@@ -7,6 +7,44 @@ namespace fixie
 {
     GLfloat fixed_to_float(GLfixed val);
     GLfixed float_to_fixed(GLfloat val);
+
+    class real
+    {
+    public:
+        real(GLfloat flt);
+        real(GLfixed fxt);
+
+        GLfloat as_float() const;
+        GLfixed as_fixed() const;
+
+    private:
+        union
+        {
+            GLfloat _float;
+            GLfixed _fixed;
+        };
+        bool _is_float;
+    };
+
+    class real_ptr
+    {
+    public:
+        real_ptr(const GLfloat* flt);
+        real_ptr(const GLfixed* fxt);
+
+        GLfloat as_float(size_t n) const;
+        GLfixed as_fixed(size_t n) const;
+
+        real operator[](size_t n) const;
+
+    private:
+        union
+        {
+            const GLfloat* _float;
+            const GLfixed* _fixed;
+        };
+        bool _is_float;
+    };
 }
 
 #endif // _FIXED_POINT_HPP_
