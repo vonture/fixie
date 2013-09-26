35bc6585b2477a99b991cdc37f8ee1a858c9d358
Geoff Lang <geoff@vonture.net>
2013-09-25 20:51:39 -0400

    Add a material class and context material memebers.

--- src/context.cpp
+++ src/context.cpp
@@ -1,1 +1,30 @@
 #include "context.hpp"
+
+namespace fixie
+{
+    context::context()
+        : _front_material(get_default_material())
+        , _back_material(get_default_material())
+    {
+    }
+
+    material& context::front_material()
+    {
+        return _front_material;
+    }
+
+    const material& context::front_material() const
+    {
+        return _front_material;
+    }
+
+    material& context::back_material()
+    {
+        return _back_material;
+    }
+
+    const material& context::back_material() const
+    {
+        return _back_material;
+    }
+}
--- src/context.hpp
+++ src/context.hpp
@@ -1,13 +1,24 @@
 #ifndef _FIXIE_CONTEXT_HPP_
 #define _FIXIE_CONTEXT_HPP_
 
-#include <memory>
+#include "material.hpp"
 
 namespace fixie
 {
     class context
     {
+    public:
+        context();
 
+        material& front_material();
+        const material& front_material() const;
+
+        material& back_material();
+        const material& back_material() const;
+
+    private:
+        material _front_material;
+        material _back_material;
     };
 }
 
