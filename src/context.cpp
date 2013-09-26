43d3ccb80d16a52468b8ad21b502925c75ac458a
Geoff Lang <geoff@vonture.net>
2013-09-25 23:19:43 -0400

    Add light members to context.

--- src/context.cpp
+++ src/context.cpp
@@ -6,6 +6,10 @@ namespace fixie
         : _front_material(get_default_material())
         , _back_material(get_default_material())
     {
+        for (size_t i = 0; i < _light_count; i++)
+        {
+            _lights[i] = get_default_light(i);
+        }
     }
 
     material& context::front_material()
@@ -26,5 +30,20 @@ namespace fixie
     const material& context::back_material() const
     {
         return _back_material;
+    }
+
+    light& context::lights(size_t idx)
+    {
+        return _lights[idx];
+    }
+
+    const light& context::lights(size_t idx) const
+    {
+        return _lights[idx];
+    }
+
+    size_t context::light_count() const
+    {
+        return _light_count;
     }
 }
--- src/context.hpp
+++ src/context.hpp
@@ -2,6 +2,7 @@
 #define _FIXIE_CONTEXT_HPP_
 
 #include "material.hpp"
+#include "light.hpp"
 
 namespace fixie
 {
@@ -16,9 +17,16 @@ namespace fixie
         material& back_material();
         const material& back_material() const;
 
+        light& lights(size_t idx);
+        const light& lights(size_t idx) const;
+        size_t light_count() const;
+
     private:
         material _front_material;
         material _back_material;
+
+        static const size_t _light_count = 8;
+        light _lights[_light_count];
     };
 }
 
