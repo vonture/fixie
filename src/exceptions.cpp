1c75a24df0aef24009292c330b924529bbba6fc4
Geoff Lang <geoff@vonture.net>
2013-09-25 20:51:50 -0400

    Added exception classes for context and gl errors.

---
+++ src/exceptions.cpp
@@ -0,0 +1,55 @@
+#include "exceptions.hpp"
+
+namespace fixie
+{
+    context_error::context_error(const std::string& msg)
+        : std::runtime_error(msg)
+    {
+    }
+
+    no_context_error::no_context_error()
+        : context_error("no context exists.")
+    {
+    }
+
+    gl_error::gl_error(GLenum error_code, const std::string& msg)
+        : std::runtime_error(msg)
+        , _error_code(error_code)
+    {
+    }
+
+    GLenum gl_error::error_code() const
+    {
+        return _error_code;
+    }
+
+    invalid_enum_error::invalid_enum_error(const std::string& msg)
+        : gl_error(GL_INVALID_ENUM, msg)
+    {
+    }
+
+    invalid_value_error::invalid_value_error(const std::string& msg)
+        : gl_error(GL_INVALID_VALUE, msg)
+    {
+    }
+
+    invalid_operation_error::invalid_operation_error(const std::string& msg)
+        : gl_error(GL_INVALID_OPERATION, msg)
+    {
+    }
+
+    stack_overflow_error::stack_overflow_error(const std::string& msg)
+        : gl_error(GL_STACK_OVERFLOW, msg)
+    {
+    }
+
+    stack_underflow_error::stack_underflow_error(const std::string& msg)
+        : gl_error(GL_STACK_UNDERFLOW, msg)
+    {
+    }
+
+    out_of_memory_error::out_of_memory_error(const std::string& msg)
+        : gl_error(GL_OUT_OF_MEMORY, msg)
+    {
+    }
+}
---
+++ src/exceptions.hpp
@@ -0,0 +1,72 @@
+#ifndef _EXCEPTIONS_HPP_
+#define _EXCEPTIONS_HPP_
+
+#include <stdexcept>
+#include <string>
+
+#include "fixie_gl_types.h"
+#include "fixie_gl_es.h"
+
+namespace fixie
+{
+    class context_error : public std::runtime_error
+    {
+    public:
+        context_error(const std::string& msg);
+    };
+
+    class no_context_error : public context_error
+    {
+    public:
+        no_context_error();
+    };
+
+    class gl_error : public std::runtime_error
+    {
+    public:
+        gl_error(GLenum error_code, const std::string& msg);
+
+        GLenum error_code() const;
+
+    private:
+        GLenum _error_code;
+    };
+
+    class invalid_enum_error : public gl_error
+    {
+    public:
+        invalid_enum_error(const std::string& msg);
+    };
+
+    class invalid_value_error : public gl_error
+    {
+    public:
+        invalid_value_error(const std::string& msg);
+    };
+
+    class invalid_operation_error : public gl_error
+    {
+    public:
+        invalid_operation_error(const std::string& msg);
+    };
+
+    class stack_overflow_error : public gl_error
+    {
+    public:
+        stack_overflow_error(const std::string& msg);
+    };
+
+    class stack_underflow_error : public gl_error
+    {
+    public:
+        stack_underflow_error(const std::string& msg);
+    };
+
+    class out_of_memory_error : public gl_error
+    {
+    public:
+        out_of_memory_error(const std::string& msg);
+    };
+}
+
+#endif // _EXCEPTIONS_HPP_
