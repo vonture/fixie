b00ac2140b0c0ba08368246a913409441b83b0b3
Geoff Lang <geoff@vonture.net>
2013-09-26 22:40:41 -0400

    Add an UNREACHABLE define.

--- src/debug.hpp
+++ src/debug.hpp
@@ -5,8 +5,10 @@
 
 #ifdef BUILD_DEBUG
 #define UNIMPLEMENTED() assert(false)
+#define UNREACHABLE() assert(false)
 #else
 #define UNIMPLEMENTED()
+#define UNREACHABLE()
 #endif
 
 #endif // _DEBUG_H_
