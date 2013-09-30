4041cd74fb32efec134899b67c0b2603d663928e
Geoff Lang <geoff@vonture.net>
2013-09-29 20:30:42 -0400

    Added stubs for fixed point conversion tests.

---
+++ test/fixed_point_tests.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+#include "fixed_point.hpp"
+
+namespace fixie
+{
+    static GLfloat epsilon = 0.001f;
+
+    TEST(fixed_point_tests, conversion)
+    {
+    }
+
+    TEST(fixed_point_tests, reals)
+    {
+    }
+}
