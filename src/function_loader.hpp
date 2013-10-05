a6edfa000847838ec3d4367f7432bbdc2e60778a
Geoff Lang <geoff@vonture.net>
2013-10-05 13:46:04 -0400

    Fixed some defines in the gl function loader.

--- src/function_loader.hpp
+++ src/function_loader.hpp
@@ -1,8 +1,20 @@
-#ifndef _FUNCTION_LOADER_HPP
-#define _FUNCTION_LOADER_HPP
+#ifndef _FUNCTION_LOADER_HPP_
+#define _FUNCTION_LOADER_HPP_
 
 #include <functional>
 #include <string>
+
+#ifndef GL_APIENTRY
+    #if defined(_WIN32)
+        #define GL_APIENTRY __stdcall
+    #else
+        #define GL_APIENTRY
+    #endif
+#endif
+
+#ifndef GL_APIENTRYP
+    #define GL_APIENTRYP GL_APIENTRY*
+#endif
 
 namespace fixie
 {
@@ -11,4 +23,4 @@ namespace fixie
 
 #include "function_loader.inl"
 
-#endif // _FUNCTION_LOADER_HPP
+#endif // _FUNCTION_LOADER_HPP_
