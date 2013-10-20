bf259485977c1f13067229f908abe3df50efc154
Geoff Lang <geoff@vonture.net>
2013-10-20 13:35:14 -0400

    The simple texturing sample now generates a fractal for it's texture.

--- samples/simple_texturing/simple_texturing.cpp
+++ samples/simple_texturing/simple_texturing.cpp
@@ -46,20 +46,56 @@ int main(int argc, char** argv)
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    const unsigned int texture_width = 256;
-    const unsigned int texture_height = 256;
+    const size_t texture_width = 1024;
+    const size_t texture_height = 1024;
+    const size_t iterations = 1024;
+
+    const float seed_x = -0.4f;
+    const float seed_y =  0.6f;
+
+    std::vector<GLubyte> color_pallete(256 * 4);
+    for (size_t i = 0; i < 256; i++)
+    {
+        size_t pixel_index = i * 4;
+
+        color_pallete[pixel_index + 0] = GLubyte((((std::min(std::max(i, 0U), 31U)) - 0U) / 32.0f) * 255);
+        color_pallete[pixel_index + 1] = GLubyte((((std::min(std::max(i, 32U), 63U)) - 32U) / 32.0f) * 255);
+        color_pallete[pixel_index + 2] = GLubyte((((std::min(std::max(i, 64U), 255U)) - 192U) / 192.0f) * 255);
+        color_pallete[pixel_index + 3] = 255;
+    }
 
     std::vector<GLubyte> texture_data(texture_width * texture_height * 4);
-    for(int y = 0; y < texture_height; ++y)
+    for(size_t y = 0; y < texture_height; ++y)
     {
-        for(int x = 0; x < texture_width; ++x)
+        for(size_t x = 0; x < texture_width; ++x)
         {
-            unsigned int pixel_index = (y * texture_height + x) * 4;
+            float homogeneous_x = ((float(x) / texture_width) - 0.5f) * 2.0f;
+            float homogeneous_y = ((float(y) / texture_width) - 0.5f) * 2.0f;
 
-            texture_data[pixel_index + 0] = 0;
-            texture_data[pixel_index + 1] = 0;
-            texture_data[pixel_index + 2] = 0;
-            texture_data[pixel_index + 3] = 0;
+            size_t i;
+            for (i = 0; i < iterations; i++)
+            {
+                float test_x = (homogeneous_x * homogeneous_x - homogeneous_y * homogeneous_y) + seed_x;
+                float test_y = (homogeneous_y * homogeneous_x + homogeneous_x * homogeneous_y) + seed_y;
+
+                if ((test_x * test_x + test_y * test_y) > 4.0f)
+                {
+                    break;
+                }
+
+                homogeneous_x = test_x;
+                homogeneous_y = test_y;
+            }
+
+            GLubyte result = (i != iterations) ? GLubyte((float(i) / iterations) * 255) : 0;
+
+            size_t pallete_index = result * 4;
+
+            size_t pixel_index = (y * texture_height + x) * 4;
+            texture_data[pixel_index + 0] = color_pallete[pallete_index + 0];
+            texture_data[pixel_index + 1] = color_pallete[pallete_index + 1];
+            texture_data[pixel_index + 2] = color_pallete[pallete_index + 2];
+            texture_data[pixel_index + 3] = color_pallete[pallete_index + 3];
         }
     }
 
@@ -86,7 +122,7 @@ int main(int argc, char** argv)
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        float scale = std::min<float>(width, height) * 0.75f;
+        float scale = std::min(width, height) * 0.9f;
         glScalef(scale, scale, scale);
         glRotatef(time * 50.f, 1.0f, 1.0f, 1.0f);
 
